085d6713f9f640d184d64e258b84613399056de3
William Camargo <wcamargo@eusou.space>
2020-03-28 13:44:44 -0300

    app com funcionalidade de incremento e decremento e mutação de texto

--- contator_de_pessoas/lib/main.dart
+++ contator_de_pessoas/lib/main.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
-
 void main(){
   runApp(MaterialApp(
     title: "Contador de Pessoas",
+    home: Home()
+  ));
+}
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+class _HomeState extends State<Home> {
+  
+  int _people = 0;
+  String _infoText = "Pode Entrar!";
+
+  void changePeople(int delta){
+    //setState atualiza tela
+    setState(() {
+      _people += delta;  
+
+      if(_people < 0){
+        _infoText = "Mundo invertido?";
+      }else if(_people <= 10){
+        _infoText = "Pode Entrar!";
+      }else{
+        _infoText = "Lotado";
+      }
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     //O Stack permite sobrepor os widgets, nesse caso a imagem com a coluna
-    home: Stack(
+    return Stack(
       children: <Widget>[
         Image.asset(
           "images/original.jpg",
@@ -12,24 +41,20 @@ void main(){
           fit: BoxFit.cover,
           height: 1000.0,
         ),
-
         //O column permite colocar um widget em cima do outro
         Column(
           //Alinhando o texto no centro da tela
           mainAxisAlignment: MainAxisAlignment.center,
-
           //children significa que pode colocar mais de um widget
           children: <Widget>[
             Text(
-              "Pessoas: 0", 
+              "Pessoas: $_people", 
               style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             ),
-
             //O Row coloca o itens um ao lado do outro
             Row(
               //Alinhamento
               mainAxisAlignment: MainAxisAlignment.center,
-
               children: <Widget>[
                 //Padding da espaços nas laterais do widget, nesse caso para o FlatButton
                 Padding(
@@ -46,17 +71,16 @@ void main(){
                     ),
                     //Função anonima
                     onPressed: () {
-
+                      changePeople(1);
                     },
                   ),
                 ),
-
                 Padding(
                   padding: EdgeInsets.all(10.0),
                   child: FlatButton(
                     //child significa que pode colocar apenas um widget
                     child: Text(
-                      "+1",
+                      "-1",
                       style: TextStyle(
                         fontStyle: FontStyle.italic,
                         fontSize: 40.0, 
@@ -65,24 +89,21 @@ void main(){
                     ),
                     //Função anonima
                     onPressed: () {
-
+                      changePeople(-1);
                     },
                   ),
                 ),
               ],
             ),
-
             Text(
-              "Pode Entrar!", 
+              _infoText, 
               style: TextStyle(color: Colors.white, 
                 fontStyle: FontStyle.italic,
                 fontSize: 30.0),
             ),
-
-
           ],
         ),
       ],
-    )
-  ));
+    );
+  }
 }
